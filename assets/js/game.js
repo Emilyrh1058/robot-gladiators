ddeaba34cbc986b4687eeaf6aafd64f0fa6d3a87
Emily Salinas <emilyrh1058@gmail.com>
2021-02-15 01:39:33 -0600

    corrected misspelling

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,7 +73,7 @@ for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
   if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert("Welcome to Robot Gladiators! Routh " + (i + 1));
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
     // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
 
